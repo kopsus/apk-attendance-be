2272f19768c6aca0e44a627e0b4a343486f2a19c
DwikiBudilaksana <budilaksana.dwiki@gmail.com>
2024-03-24 18:58:29 +0700

    cleanup code

--- src/index.ts
+++ src/index.ts
@@ -4,10 +4,8 @@ import adminRouter from './router/adminRouter'
 import Logger from './util/loggerUtil'
 import { initDbConnection } from './accessor/databaseConnectionInitializer'
 import { initModel } from './model/modelInitializer'
-import { employeeEntity } from './model/employeeEntity'
 import LoggerUtil from './util/loggerUtil'
-import bcrypt from 'bcrypt'
-import jwt from 'jsonwebtoken'
+import userRouter from './router/userRouter'
 
 const DOMAIN = 'ROOT'
 
@@ -28,83 +26,13 @@ initDbConnection()
 
 app.use(express.json())
 
-app.post('/signup', async function (req: Request, res: Response) {
-
-    try {
-        // Hash the password
-        const hashedPassword = await bcrypt.hash(req.body.password, 10)
-
-        // Insert a new user into the database
-        const newUser = await employeeEntity.create({
-            email: req.body.email,
-            password: hashedPassword,
-            company_id: req.body.company_id,
-        })
-
-        // Response status code 200
-        res.send({
-            success: true,
-            data: {},
-            message: 'Data Created',
-            code: 200,
-        })
-    } catch (error) {
-        res.status(500).json({ error: 'Internal Server Error' })
-    }
-})
-
-app.post('/login', async function (req: Request, res: Response) {
-
-    // Checking Header Authorization
-    // if (req.headers.authorization !== 'EternalPlus@100') {
-    //     res.status(401).json({ error: 'Unauthorized' })
-    // }
-
-    try {
-        // Checking if the user exists in the database
-        const user = await employeeEntity.findOne({
-            attributes: ['id', 'password'],
-            where: {
-                email: req.body.email,
-            },
-        })
-
-        const hashedPassword = user?.get('password')
-
-        // Check if the password is valid
-        if (typeof hashedPassword !== 'string' || hashedPassword === null) {
-            res.status(400).send({ error: 'Invalid Password' })
-            return
-        }
-
-        // Compare the password
-        const match = await bcrypt.compare(req.body.password, hashedPassword)
-        if (match) {
-            const access_token = jwt.sign({ userId: user?.get('id') }, 'EternalPlus@100', {
-                expiresIn: '1w',
-                });
-
-            res.send({
-                success: true,
-                data: {access_token},
-                message: 'Login Successfully',
-                code: 200,
-            })
-        } else {
-            res.status(401).json({ error: 'Invalid Email or Password' })
-        }
-    } catch (err) {
-        console.error('Error authenticating user:', err)
-        res.status(500).json({ error: 'Internal Server Error' })
-    }
-})
-
 app.get('/healthcheck', (req: Request, res: Response) => {
     res.send('Healthy!')
 })
 
 // Use the external router for the specific path
 app.use('/admin', adminRouter)
+app.use('/user', userRouter)
 
 app.use('*', (req: Request, res: Response) => {
     Logger.log(DOMAIN, `Unhandled path: ${req.originalUrl}`)
